3147fa6bb2c3b003f85cbea7585e7bb3d5d255b8
SutandoTsukai181 <52977072+SutandoTsukai181@users.noreply.github.com>
2020-08-12 14:25:53 +0300

    Normalize whitespaces

--- yk2_decrypt.c
+++ yk2_decrypt.c
@@ -8,21 +8,21 @@ static const int  KeyLen = 13;
 
 int main(int argc, char **argv)
 {
-	FILE *in, *out;
-	char *data, *outname, *filename, *ext;
-	size_t sz;
-	
-	if (argc != 2)
-	{
-		printf("Usage: %s in.sav\n", argv[0]);
-		return 0;
-	}
-	
-	if ((in = fopen(argv[1], "rb")) == NULL)
-	{
-		perror("Error");
-		return 1;
-	}
+    FILE *in, *out;
+    char *data, *outname, *filename, *ext;
+    size_t sz;
+    
+    if (argc != 2)
+    {
+        printf("Usage: %s in.sav\n", argv[0]);
+        return 0;
+    }
+    
+    if ((in = fopen(argv[1], "rb")) == NULL)
+    {
+        perror("Error");
+        return 1;
+    }
     
     filename = malloc(sizeof(argv[1]));
     strcpy(filename, argv[1]);
@@ -31,33 +31,33 @@ int main(int argc, char **argv)
     
     outname = strcat(filename, ".json");
     
-	if ((out = fopen(outname, "wb")) == NULL)
-	{
-		perror("Error");
-		fclose(in);
-		return 1;
-	}
-	
-	printf("Writing %s to %s...\n", argv[1], outname);
-	
-	fseek(in, 0, SEEK_END);
-	sz = ftell(in) - 4;
-	data = malloc(sz);
-	rewind(in);
-	fread(data, 1, sz, in);
-	fclose(in);
-	
-	for (int i = 0; i < sz; i++)
-		data[i] ^= Key[i % KeyLen];
+    if ((out = fopen(outname, "wb")) == NULL)
+    {
+        perror("Error");
+        fclose(in);
+        return 1;
+    }
+    
+    printf("Writing %s to %s...\n", argv[1], outname);
+    
+    fseek(in, 0, SEEK_END);
+    sz = ftell(in) - 4;
+    data = malloc(sz);
+    rewind(in);
+    fread(data, 1, sz, in);
+    fclose(in);
+    
+    for (int i = 0; i < sz; i++)
+        data[i] ^= Key[i % KeyLen];
     
     // remove checksum from end of file to get proper json
-	
-	fwrite(data, 1, sz, out);
-	
-	free(data);
-	fclose(out);
-	
-	puts("Done!");
-	
-	return 0;
+    
+    fwrite(data, 1, sz, out);
+    
+    free(data);
+    fclose(out);
+    
+    puts("Done!");
+    
+    return 0;
 }
--- yk2_encrypt.c
+++ yk2_encrypt.c
@@ -104,27 +104,27 @@ step4:
     
 finish: 
     checksum = (checksum << 0x10) | add;
-	return checksum;
+    return checksum;
 }
 
 int main(int argc, char **argv)
 {
-	FILE *in, *out;
-	unsigned char *data, *outname, *filename, *ext;
-	size_t sz;
+    FILE *in, *out;
+    unsigned char *data, *outname, *filename, *ext;
+    size_t sz;
     uint32_t checksum;
-	
-	if (argc != 2)
-	{
-		printf("Usage: %s in.json\n", argv[0]);
-		return 0;
-	}
-	
-	if ((in = fopen(argv[1], "rb")) == NULL)
-	{
-		perror("Error");
-		return 1;
-	}
+    
+    if (argc != 2)
+    {
+        printf("Usage: %s in.json\n", argv[0]);
+        return 0;
+    }
+    
+    if ((in = fopen(argv[1], "rb")) == NULL)
+    {
+        perror("Error");
+        return 1;
+    }
     
     // get filename without extension
     filename = malloc(sizeof(argv[1]));
@@ -134,28 +134,28 @@ int main(int argc, char **argv)
     
     outname = strcat(filename, ".sav");
     
-	if ((out = fopen(outname, "wb")) == NULL)
-	{
-		perror("Error");
-		fclose(in);
-		return 1;
-	}
-	
-	printf("Writing %s to %s...\n", argv[1], outname);
-	
-	fseek(in, 0, SEEK_END);
-	sz = ftell(in);
-	data = malloc(sz + 4);
-	rewind(in);
-	fread(data, 1, sz, in);
-	fclose(in);
+    if ((out = fopen(outname, "wb")) == NULL)
+    {
+        perror("Error");
+        fclose(in);
+        return 1;
+    }
+    
+    printf("Writing %s to %s...\n", argv[1], outname);
+    
+    fseek(in, 0, SEEK_END);
+    sz = ftell(in);
+    data = malloc(sz + 4);
+    rewind(in);
+    fread(data, 1, sz, in);
+    fclose(in);
     
     // calculate checksum
     checksum = calculate(data, (uint64_t)sz);
     
     // encrypt
-	for (int i = 0; i < sz; i++)
-		data[i] ^= Key[i % KeyLen];
+    for (int i = 0; i < sz; i++)
+        data[i] ^= Key[i % KeyLen];
     
     // add checksum
     data[sz] = checksum;
@@ -163,12 +163,12 @@ int main(int argc, char **argv)
     data[sz + 2] = checksum >> 16;
     data[sz + 3] = checksum >> 24;
     
-	fwrite(data, 1, sz + 4, out);
-	
-	free(data);
-	fclose(out);
-	
-	puts("Done!");
-	
-	return 0;
+    fwrite(data, 1, sz + 4, out);
+    
+    free(data);
+    fclose(out);
+    
+    puts("Done!");
+    
+    return 0;
 }
